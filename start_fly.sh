d6dc81df339145c810c8e375d4a848fa46303893
Mark Iannucci <mark.iannucci@gmail.com>
2023-07-27 11:57:24 -0600

    logtostderr

--- start_fly.sh
+++ start_fly.sh
@@ -5,7 +5,7 @@ set -eu
 echo "Starting on Fly ..."
 exec /cockroach/cockroach start \
   --insecure \
-  --logstostderr \
+  --logtostderr \
   --locality=region=$FLY_REGION \
   --cluster-name=$FLY_APP_NAME \
   --join=$FLY_APP_NAME.internal:36257 \
