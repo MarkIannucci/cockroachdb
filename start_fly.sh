a44bc9294ddd2bfd022613d9ea515c532a0ec138
Mark Iannucci <mark.iannucci@gmail.com>
2023-07-27 10:57:24 -0600

    Add more from this forum post https://community.fly.io/t/cockroachdb-example-wont-scale-to-multiple-instances/2006/9

--- start_fly.sh
+++ start_fly.sh
@@ -5,11 +5,20 @@ set -eu
 echo "Starting on Fly ..."
 echo exec /cockroach/cockroach start \
   --insecure \
+  --logtostderr \
   --locality=fly_region=$FLY_REGION \
   --cluster-name=$FLY_APP_NAME \
-  --join=$FLY_APP_NAME.internal
+  --join=$FLY_APP_NAME.internal \
+  --listen-addr=:36257 \
+  --sql-addr:26257 \
+  --advertise-addr=$(hostname -f) \
 exec /cockroach/cockroach start \
   --insecure \
+  --logtostderr \
   --locality=fly_region=$FLY_REGION \
   --cluster-name=$FLY_APP_NAME \
-  --join=$FLY_APP_NAME.internal
+  --join=$FLY_APP_NAME.internal \
+  --listen-addr=:36257 \
+  --sql-addr:26257 \
+  --advertise-addr=$(hostname -f) \
+  
