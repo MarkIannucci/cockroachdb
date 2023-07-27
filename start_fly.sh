20cbef73100340c859426cc04b89abad4642436b
Mark Iannucci <mark.iannucci@gmail.com>
2023-07-27 11:40:08 -0600

    join port should be same as listen?

--- start_fly.sh
+++ start_fly.sh
@@ -7,7 +7,7 @@ exec /cockroach/cockroach start \
   --insecure \
   --locality=fly_region=$FLY_REGION \
   --cluster-name=$FLY_APP_NAME \
-  --join=$FLY_APP_NAME.internal \
+  --join=$FLY_APP_NAME.internal:36257 \
   --listen-addr=:36257 \
   --sql-addr=:26257 \
   --advertise-addr=$(hostname -f) \
