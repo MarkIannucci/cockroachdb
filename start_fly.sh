2bb0e6a27b955e16d830dad1465d9d5f4fc2da2d
Mark Iannucci <mark.iannucci@gmail.com>
2023-07-27 11:51:34 -0600

    Fix WARNING: The --locality flag does not contain a "region" tier. To to databases, the --locality flag must contain a "region" tier.add regions

--- start_fly.sh
+++ start_fly.sh
@@ -5,7 +5,7 @@ set -eu
 echo "Starting on Fly ..."
 exec /cockroach/cockroach start \
   --insecure \
-  --locality=fly_region=$FLY_REGION \
+  --locality=region=$FLY_REGION \
   --cluster-name=$FLY_APP_NAME \
   --join=$FLY_APP_NAME.internal:36257 \
   --advertise-addr=$(hostname -f) \
